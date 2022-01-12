17875169a9d064e6518fb091e211baae6acbfecf
Eddishot <Eddi_sl94@hotmail.it>
2022-01-12 23:51:32 +0100

    exercise completed

--- database/migrations/2022_01_11_222543_create_travels_table.php
+++ database/migrations/2022_01_11_222543_create_travels_table.php
@@ -15,10 +15,10 @@ class CreateTravelsTable extends Migration
     {
         Schema::create('travels', function (Blueprint $table) {
             $table->id();
-            $table->string(`airport`, 100);
-            $table->string(`country`, 100)->afetr(`id`);
-            $table->text(`description`);
-            $table->string(`airline`, 100);
+            $table->string("airport", 100);
+            $table->string("country", 100)->afetr("id");
+            $table->text("description");
+            $table->string("airline", 100);
             $table->timestamps();
         });
     }
